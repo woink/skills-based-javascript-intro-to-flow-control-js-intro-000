1d25edcc576d8a6c58c94de8ff5c83c3cd5d4946
kind-proxy-9572 <wardprice@gmail.com>
2020-06-08 18:37:50 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,7 +29,7 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch(age) {
     case (age >= 13 && age <= 19):
-      return "You are a teenager";
+      return "You are a teenger";
     default:
       return "You have an age"
   }
