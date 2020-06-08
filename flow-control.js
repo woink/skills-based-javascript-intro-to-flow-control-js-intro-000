de23d970a6b48cccc77ac8b4654ca078d390697c
kind-proxy-9572 <wardprice@gmail.com>
2020-06-08 18:41:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,7 +30,8 @@ function switchAge(age) {
   switch(age) {
     case age >= 13 && age <= 19:
       return "You are a teenager";
+      break;
     default:
-      return "You dhave an age"
+      return "You have an age"
   }
 }
